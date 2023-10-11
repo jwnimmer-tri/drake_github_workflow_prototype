71d173589bedcaa0f7c2590a83e408bad0c42869
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-10-11 12:52:03 -0700

    mkdir

--- .github/ci_build.sh
+++ .github/ci_build.sh
@@ -2,6 +2,10 @@
 
 set -euxo pipefail
 
+chmod 1777 /opt
+mkdir /opt/drake-wheel
+chmod 1777 /opt/drake-wheel
+
 cd drake
 bazel run //tools/wheel:builder -- \
   --output-dir /opt/drake-wheel 0.0.0
