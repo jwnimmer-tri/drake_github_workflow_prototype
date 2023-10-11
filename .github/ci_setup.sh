d1e9765fed567a02e55719ab7e6bf6cc88d14d70
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-10-11 14:33:36 -0700

    actually use the rcfile

--- .github/ci_setup.sh
+++ .github/ci_setup.sh
@@ -2,14 +2,12 @@
 
 set -euxo pipefail
 
-pwd
-
 brew analytics off
 brew update || (sleep 15; brew update)
 brew bundle --file="${BASH_SOURCE%/*}/Brewfile.wheel" --no-lock
 
 drake/setup/mac/source_distribution/install_prereqs_user_environment.sh
 
-ln -s .github/ci.bazelrc drake/user.bazelrc
+ln -s .github/ci.bazelrc ~/.bazelrc
 
 ln -s $(which gfortran-12) /usr/local/bin/gfortran
