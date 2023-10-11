6204451c9a431779847e929548b9d0c3841ae679
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-10-11 15:11:19 -0700

    yes, it was the link paths; bah

--- .github/ci_build.sh
+++ .github/ci_build.sh
@@ -7,10 +7,5 @@ mkdir /opt/drake-wheel
 sudo chmod 1777 /opt/drake-wheel
 
 cd drake
-
-bazel info
-bazel info --announce_rc
-false
-
 bazel run //tools/wheel:builder -- \
   --output-dir /opt/drake-wheel 0.0.0
