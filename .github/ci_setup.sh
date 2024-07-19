c8360346f0f8a37b94bf5716c1431a54cfb30008
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-07-19 10:42:38 -0700

    put back the homebrew symlink you github morons

--- .github/ci_setup.sh
+++ .github/ci_setup.sh
@@ -5,6 +5,7 @@ set -euxo pipefail
 brew analytics off
 brew update || (sleep 15; brew update)
 brew bundle --file="${BASH_SOURCE%/*}/Brewfile.wheel" --no-lock
+ln -s /usr/local/bin/gfortran-14 /usr/local/bin/gfortran
 pip3.12 install -r "${BASH_SOURCE%/*}/requirements.wheel"
 
 drake/setup/mac/source_distribution/install_prereqs_user_environment.sh
