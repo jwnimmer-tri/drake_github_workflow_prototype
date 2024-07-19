d110f20f4c4dec5be1d294cdb7b7e86d00aa91b5
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-07-19 10:38:52 -0700

    debugging

--- .github/ci_setup.sh
+++ .github/ci_setup.sh
@@ -7,6 +7,10 @@ brew update || (sleep 15; brew update)
 brew bundle --file="${BASH_SOURCE%/*}/Brewfile.wheel" --no-lock
 pip3.12 install -r "${BASH_SOURCE%/*}/requirements.wheel"
 
+# XXX debugging
+ls -l /usr/local/bin
+ls -l /usr/local/bin/gfortran
+
 drake/setup/mac/source_distribution/install_prereqs_user_environment.sh
 
 ln -s $(pwd)/.github/ci.bazelrc ~/.bazelrc
